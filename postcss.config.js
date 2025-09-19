ad686b6b5c62e63e32fc0e77f6f4d2ebd9458986
unknown <info@briskbear.ca>
2025-09-19 16:44:31 -0400

    Auto-update 2025-09-19 16:44:31

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     require('cssnano')({ preset: 'default' }),
     require('postcss-url')({
       url: 'inline', // inline assets instead of keeping as URL
-      maxSize: 14,   // in KB, default is 14, set higher if your SVGs are bigger
+      maxSize: 10,   // in KB, default is 14, set higher if your SVGs are bigger
       fallback: 'copy' // what to do if file is too large
     }),
   ]
